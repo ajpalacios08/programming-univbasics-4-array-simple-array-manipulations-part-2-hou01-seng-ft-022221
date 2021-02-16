49503ff48fe63bf3c69b573988a01155345bf5e6
Allen Palacios <apalacios1994@gmail.com>
2021-02-16 04:19:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,6 +29,7 @@ def using_insert(list_of_programming_languages, another_language)
  
  def using_delete(instructors, string)
    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-   no_offense_steven = instructors.delete("Steven")
    instructors.delete("Steven")
+   no_offense_steven = instructors.delete("Steven")
+   instructors
    end
